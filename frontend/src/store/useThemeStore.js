e2397d5e5cd2386570672f5a3edfd0c85bd1ab58
emiloumeshg <emiloumeshg@emilo.in>
2026-06-03 10:29:07 +0530

    fixed the theem bug

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,14 +1,14 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set, get) => ({
-  theme: localStorage.getItem("streamify-theme") || "streamify-pro",
+  theme: localStorage.getItem("streamify-theme") || "MeetFlow-pro",
   setTheme: (theme) => {
     localStorage.setItem("streamify-theme", theme);
     set({ theme });
   },
   toggleTheme: () => {
     const currentTheme = get().theme;
-    const nextTheme = currentTheme === "streamify-pro" ? "streamify-dark" : "streamify-pro";
+    const nextTheme = currentTheme === "MeetFlow-pro" ? "MeetFlow-dark" : "MeetFlow-pro";
     localStorage.setItem("streamify-theme", nextTheme);
     set({ theme: nextTheme });
   },
